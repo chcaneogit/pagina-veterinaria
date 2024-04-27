02ee84430ab6484e8be263e85d5be64a73282503
Christian Caneo <caneovilches46@gmail.com>
2024-04-27 12:13:09 -0400

    Validaciones

--- main.js
+++ main.js
@@ -2,14 +2,36 @@ function validarNombre(idCampo) {
     let nombreInput = document.getElementById(idCampo);
     let nombre = nombreInput.value;
     let nombreError = document.getElementById(idCampo + "-error");
+    let formulario = nombreInput.closest('form');
 
     if (nombre.length <=3) {
-        nombreError.innerText = "Por favor, ingresa tu nombre.";
+        nombreError.innerText = "Por favor, ingresa tu "+ idCampo;
+        formulario.classList.remove('was-validated');
         return false;
     } else {
         nombreError.innerText = "";
-        nombreInput.classList.remove('is-invalid'); // Quitar la marca de inválido
+        formulario.classList.add('was-validated');
         return true;
     }
 }
+
+function validarEdad(idCampo) {
+    let fechaNacimientoInput = document.getElementById(idCampo);
+    let fechaNacimiento = new Date(fechaNacimientoInput.value);
+    let hoy = new Date();
+    
+    let segundos = hoy - fechaNacimiento;
+
+    let meses = segundos / (1000 * 60 * 60 * 24 * 30);
+
+    let edad = Math.trunc(meses/12);
+    
+
+    
+
+    console.log("Edad calculada:", edad, "años");
+
+    
+}
+
 
